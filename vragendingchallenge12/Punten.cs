b5982c0d1925861d6dbf585ecd4c195ec8b93e82
BramNoortman <91888756+BramNoortman@users.noreply.github.com>
2022-06-22 10:09:17 +0100

    punten systeem

---
+++ vragendingchallenge12/Punten.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vragendingchallenge12
+{
+    public static class Punten
+    {
+        private static double totalPoints = 0;
+        private static List<string> punten = new List<string>();
+        public static double GetTotal()
+        {
+            return totalPoints;
+        }
+
+        public static void AddProduct(string name, double failed)
+        {
+            totalPoints = totalPoints + failed;
+            punten.Add(name);
+        }
+
+
+    }
+}
